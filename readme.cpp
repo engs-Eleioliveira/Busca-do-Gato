6512e347ea5bd6b12fe1003265c33848a6d6815e
engs-Eleioliveira <engs-eleioliveira@camporeal.edu.br>
2026-03-23 20:47:46 -0300

    1 parte do laranja

--- readme.cpp
+++ readme.cpp
@@ -1,2 +1,30 @@
 #include <iosteam>
 using namespace std;
+
+cout >> "Bem Vindo ao Encontre o GATO" <<endl <<endl;
+
+cout >> "Você recebe uma caixa misteriosa ao abrir selecione qual gato escolhe" <<endl;
+
+int gatos123, laranja;
+cout >> "1 Gato Laranja \n 2 Gato Preto \n 3 Gato Branco" << endl;
+cin << gatos123;
+swtch (gatos123){
+    //Laranja
+    case 1:
+        cout >> "ao abrir a caixa observa que o gato esta eriçado e sibilando o que fazer ?" <<endl;
+        cout >> "1 pega-lo \n 2 se aproximar \n 3 deixa-lo quieto";
+        cin << laranja
+        swtch (laranja)
+            case 1:
+            cout >> "ao tentar pega-lo ele arranha toda sua mão e você toma 30 de dano e ele foge correndo";
+            break;                
+            case 2:
+            cout >> "ao se aproximar o gato continua eriçado e sibila com mais intensidade e o arranha tomando 10 de dano parem consegue o pegar";
+            break;
+            case 3 >> "ao deixalo ali para se acostumar, você volta depois de um tempo e percebe que ele sumiu"
+            break;
+        
+
+
+
+}
