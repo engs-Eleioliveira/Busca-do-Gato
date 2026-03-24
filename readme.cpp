fcc447ac61b52f09053d622028ebcbb1a8736b3e
engs-Eleioliveira <engs-eleioliveira@camporeal.edu.br>
2026-03-23 21:06:48 -0300

    correção

--- readme.cpp
+++ readme.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 using namespace std;
 
-cout >> "Bem Vindo ao Encontre o GATO" <<endl <<endl;
+int main() {
+    cout << "Bem Vindo ao Encontre o GATO" << endl << endl;
 
-cout >> "Você recebe uma caixa misteriosa ao abrir selecione qual gato escolhe" <<endl;
+    cout << "Você recebe uma caixa misteriosa ao abrir selecione qual gato escolhe" << endl;
 
-int gatos123, laranja;
-cout >> "1 Gato Laranja \n 2 Gato Preto \n 3 Gato Branco" << endl;
-cin << gatos123;
-swtch (gatos123){
-    //Laranja
-    case 1:
-        cout >> "ao abrir a caixa observa que o gato esta eriçado e sibilando o que fazer ?" <<endl;
-        cout >> "1 pega-lo \n 2 se aproximar \n 3 deixa-lo quieto";
-        cin << laranja
-        swtch (laranja)
-            case 1:
-            cout >> "ao tentar pega-lo ele arranha toda sua mão e você toma 30 de dano e ele foge correndo";
-            break;                
-            case 2:
-            cout >> "ao se aproximar o gato continua eriçado e sibila com mais intensidade e o arranha tomando 10 de dano parem consegue o pegar";
+    int gatos123, laranja;
+    cout << "1 Gato Laranja \n2 Gato Preto \n3 Gato Branco" << endl;
+    cin >> gatos123;
+
+    switch (gatos123) {
+        // Laranja
+        case 1:
+            cout << "Ao abrir a caixa observa que o gato esta eriçado e sibilando, o que fazer?" << endl;
+            cout << "1 pega-lo \n2 se aproximar \n3 deixa-lo quieto" << endl;
+            cin >> laranja;
+
+            switch (laranja) {
+                case 1:
+                    cout << "Ao tentar pega-lo ele arranha sua mão e você toma 30 de dano e ele foge correndo." << endl;
+                    break;
+
+                case 2:
+                    cout << "Ao se aproximar o gato sibila mais e te arranha, você toma 10 de dano mas consegue pegá-lo." << endl;
+                    break;
+
+                case 3:
+                    cout << "Ao deixá-lo ali, você volta depois e percebe que ele sumiu." << endl;
+                    break;
+            }
             break;
-            case 3 >> "ao deixalo ali para se acostumar, você volta depois de um tempo e percebe que ele sumiu"
-            break;
-        
+    }
 
-
-
+    return 0;
 }
